64178f90543fa6c0dc6e9da7f016afb6474999d9
yvah <55929630+yvah@users.noreply.github.com>
2023-01-23 15:03:14 +0000

    Add files via upload

--- routerClass.py
+++ routerClass.py
@@ -19,14 +19,14 @@ class color:
 
 class Router:
 
-    def __init__(self, ipv4: str, port: int, delay: float, buffer_size: int, Controller: tuple, rec_list):
+    def __init__(self, ipv4: str, port: int, delay: float, buffer_size: int, Controller: tuple, receivers):
         self.router_ip: str = ipv4
         self.router_port: int = port
         self.router_ip_and_port: tuple = (ipv4, port)
         self.delay: float = delay
         self.buffer_size: int = buffer_size
         self.Controller = Controller
-        self.receivers_list = rec_list
+        self.receivers_dict = receivers
         self.receiver_address = None
         
         self.socket: socket.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -40,7 +40,7 @@ class Router:
     def start(self):
         self.socket.bind(self.router_ip_and_port)
         self.continue_listen = True
-        self.thread_listen = threading.Thread(target=self.__listen(self.receivers_list))
+        self.thread_listen = threading.Thread(target=self.__listen(self.receivers_dict))
         self.thread_listen.start()
         print(f'Socket up and listening at {self.router_ip}:{self.router_port}.')
 
@@ -52,18 +52,19 @@ class Router:
         exit()
 
     # checking the buffer for received bytes
-    def __listen(self, receivers_list):
+    def __listen(self):
         while self.continue_listen:
             time.sleep(self.delay)
             buffer, _ = self.socket.recvfrom(self.buffer_size)
             self.queue.put(buffer)
+            # separating the message
             node_data = buffer.decode("utf-8").split("|")
             receiver_address = node_data[0]
             message = node_data[1]
-            # checking if the receiver address is in the list of receivers
+            # checking if the receiver address is in the dictionary of receivers
             try:
-                if receiver_address not in receivers_list:
-                    self.__update_table(receiver_address, receivers_list)
+                if receiver_address not in self.receivers_dict:
+                    self.__update_table(receiver_address)
                     print(f'Unknown address {receiver_address}. Updating the table...')
                 self.__send_message(receiver_address, message)
                 print(f'Sending the message to receiver {receiver_address}.')
@@ -77,21 +78,13 @@ class Router:
     # sending the unknown address to the Controller,
     # receiving update about this address,
     # adding the address to table.
-    def __update_table(self, receiver_address, receivers_list):
+    def __update_table(self, receiver_address):
         try:
             self.socket.sendto(receiver_address, self.Controller)
             time.sleep(self.delay)
             buffer, _ = self.socket.recvfrom(self.buffer_size)
             self.queue.put(buffer)
-            # open and append the new address info:
-            receivers_file = open('receivers_list.txt', 'a')
-            receivers_file.write(buffer.decode('utf-8'))
-            receivers_file.close()
-            # open and read the file after the appending:
-            receivers_list = receivers_list.append(buffer)
-            li = open('receivers_list.txt', 'r')
-            print(f'Address added to the table: {li.read()}.')
-            receivers_file.close()
+            self.receivers_dict.update(buffer.decode('utf-8'))
         except Exception as e:
             print(f'Problem occurred while updating table: {e}.')
 
